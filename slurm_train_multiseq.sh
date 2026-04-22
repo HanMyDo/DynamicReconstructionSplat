a7fea2f01a01d31b16009ed9bdca454ee552c2a5
Han My <Han-My.Do@outlook.de>
2026-04-22 18:19:29 +0200

    exclude node essen

--- slurm_train_multiseq.sh
+++ slurm_train_multiseq.sh
@@ -8,7 +8,7 @@
 #SBATCH --mem=60G
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
-#SBATCH --exclude=essen,koblenz,essen.garching.camp.cluster,koblenz.garching.camp.cluster
+#SBATCH --exclude=essen,koblenz
 #SBATCH --time=23:00:00
 
 export ENROOT_RUNTIME_PATH=/tmp/$USER/runtime
